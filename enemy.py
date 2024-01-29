7fd400104c32cb15a5db25080387d083becc4299
eric <ericwduarte@gmail.com>
2024-01-28 21:24:20 -0300

    enemy can die now

--- enemy.py
+++ enemy.py
@@ -13,9 +13,17 @@ class Enemy():
 
     def atack(self):
         damage = random.randrange(self.atk - self.variation, self.atk + self.variation)
+        print(f'{self.name} attacked you for {damage} damage!')
         return damage
     
     def receive_damage(self, dmg):
         damage = dmg - self.df
         self.hp -= damage
-        return damage
+        if self.hp <= 0:
+            self.die()
+            return[True, self.xp]
+        print(f'{self.name} received {damage} damage!')        
+        return[False, damage]
+    
+    def die(self):
+        print(f'{self.name} died!')
